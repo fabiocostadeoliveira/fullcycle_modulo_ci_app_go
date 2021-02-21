a37d0cf6c1bdb21dc25cf581f1adf48c4e83b68b
fabiocostadeoliveira <fabiouniopar@gmail.com>
2021-02-21 07:14:25 -0300

    Primeiro commit

---
+++ sum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sum2(a int, b int) int {
+	return a + b
+}
+
+func sum3(a int, b int) int {
+	return a + b
+}
+
+func sum4(a int, b int) int {
+	return a + b
+}
